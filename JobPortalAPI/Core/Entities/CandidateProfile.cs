0741265f92478a429c7023c18ed5d2f93456f4dd
tadasviliusis <90383964+tadasviliusis@users.noreply.github.com>
2026-03-16 19:11:00 +0200

    Base entities

---
+++ JobPortalAPI/Core/Entities/CandidateProfile.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortalAPI.Core.Entities;
+
+[Table("CandidateProfiles")]
+public class CandidateProfile
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    [Required]
+    [Column("user_id")]
+    public int UserId { get; set; }
+    [Required]
+    [Column("summary")]
+    public string Summary { get; set; } = string.Empty;
+    [Required]
+    [Column("location")]
+    public string Location { get; set; } = string.Empty;
+    [Required]
+    [Column("years_of_experience")]
+    public int YearsOfExperience { get; set; }
+    
+    public virtual ICollection<CandidateSkill> CandidateSkills { get; set; } = new List<CandidateSkill>();
+}
---
+++ JobPortalAPI/Core/Entities/EmployerProfile.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortalAPI.Core.Entities;
+
+[Table("EmployerProfiles")]
+public class EmployerProfile
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    [Required]
+    [Column("user_id")]
+    public int UserId { get; set; }
+    [Required]
+    [Column("company_name")]
+    public string CompanyName { get; set; } = string.Empty;
+    [Required]
+    [Column("location")]
+    public string Location { get; set; } = string.Empty;
+}
