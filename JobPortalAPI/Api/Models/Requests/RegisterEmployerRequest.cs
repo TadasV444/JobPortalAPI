5751eccc274fc1c176a59cd9d8572fc1d18b9437
tadasviliusis <90383964+tadasviliusis@users.noreply.github.com>
2026-03-23 16:24:14 +0200

    dtos for register/login + designer.cs

---
+++ JobPortalAPI/Api/Models/Requests/RegisterEmployerRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace JobPortalAPI.Api.Models.Requests;
+
+public class RegisterEmployerRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+}
