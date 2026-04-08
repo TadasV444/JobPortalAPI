2afeba96072d955184e2fd64faf7a585d1d0d62f
tadasviliusis <90383964+tadasviliusis@users.noreply.github.com>
2026-04-08 16:41:33 +0300

    intial dtos and a service

---
+++ JobPortalAPI/Api/Models/Requests/CreateJobRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace JobPortalAPI.Api.Models.Requests;
+
+public class CreateJobRequest
+{
+    public int EmployerProfileId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public decimal SalaryFrom { get; set; }
+    public decimal SalaryTo { get; set; }
+    
+}
---
+++ JobPortalAPI/Core/Interfaces/IJobService.cs
@@ -0,0 +1,12 @@
+﻿using JobPortalAPI.Api.Models.Requests;
+using JobPortalAPI.Api.Models.Responses;
+
+namespace JobPortalAPI.Core.Interfaces;
+
+public interface IJobService
+{
+    Task<CreateJobRequest> CreateJobAsync(CreateJobRequest request);
+    Task<UpdateJobRequest> UpdateJobAsync(int id, UpdateJobRequest request);
+    Task<List<JobResponse>> GetJobsAsync();
+    Task<JobResponse> GetJobByIdAsync(int id);
+}
